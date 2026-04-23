a1f983ef2ad93b7712172e6ef18a864fb2304274
sajid-ahsan-seyam-26 <sajidahsanseyamstudent@gmail.com>
2026-04-23 09:28:37 +0600

    Add files via upload

---
+++ DataTypeDemo.java
@@ -0,0 +1,26 @@
+/*public class DataTypeDemo {
+    public static void main(String[] args) {
+        int age = 26;
+        double price = 499.99;
+        char grade = 'A';
+        boolean active = true;
+        String name = "Sami";
+
+        System.out.println(name + " is " + age + " years old.");
+        System.out.println("Price: " + price);
+        System.out.println("Grade: " + grade);
+        System.out.println("Active: " + active);
+    }
+}
+*/
+// type castinng 
+public class DataTypeDemo {
+    public static void main(String[] args) {
+        int x = 10;
+        double y = x;
+        double price = 99.8;
+        int p = (int) price;
+        System.out.println(y);
+        System.out.println(p);
+    }
+}
